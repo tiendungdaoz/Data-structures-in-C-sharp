b9132c5446ad43b846f3e4d6c0e86dd3214b38ef
Dao Tien Dung <daotiendung0607@gmai.com>
2023-10-25 10:07:32 +0700

    Add Bai7_Generic

---
+++ Bai7_Generic/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai7_Generic
+{
+    internal class MyGeneric<T>
+    {
+        private T[] items;
+        //Ham khoi tao
+        public MyGeneric(int size)
+        { 
+            items = new T[size]; 
+        }
+        //Property
+        public T[] Items { get => items; set => items = value; }
+        
+        public T GetByInDex(int inDex)
+        {
+            if (inDex < 0 || inDex > items.Length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            {
+                return items[inDex];
+            }
+        }
+
+        public void SetItemsValue(int inDex, T value)
+        {
+            if (inDex < 0 || inDex > items.Length )
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else
+            { items[inDex] = value; }
+        }
+    }
+}
---
+++ Bai7_Generic/Program.cs
@@ -0,0 +1,45 @@
+﻿using Bai7_Generic;
+
+namespace GenericTrongCsharp
+{
+    class Program
+    {
+        //Swap 2 so nguyen
+        static void Swap(ref int a,ref int b)
+        {
+            int temp;
+            temp = a;
+            a = b;
+            b= temp;
+        }
+
+        //Swap 2 so co kieu du lieu bat ky bang viec sd Generic
+        static void Swap2<T>(ref T a,ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+        static void Main(string[] args) 
+        {
+            int a = 9; int b = 6;
+            double c = 9.4, d = 10.5;
+
+            //Sd Generic cho phuong thuc
+            Swap2<int>(ref a,ref b);
+            Swap2<double>(ref c,ref d);
+
+            //Sd Generic cho lop
+            MyGeneric<int> myGe = new MyGeneric<int>(10);
+            myGe.SetItemsValue(0, 10);
+            myGe.SetItemsValue(1, 100);
+            myGe.Items[3] = 15;
+            Console.WriteLine("So luong phan tu trong thuoc tinh items cua doi tuong myGe: {0}", myGe.Items.Length);
+            Console.WriteLine("Cac phan tu trong mang: ");
+            for (int i = 0; i < myGe.Items.Length; i++)
+            {
+                Console.WriteLine(myGe.GetByInDex(i));
+            }
+        }
+    }
+}
