6dd2fad4e278795e4f3c9bd90c829508091efd4e
Dao Tien Dung <daotiendung0607@gmai.com>
2023-11-02 15:05:18 +0700

    Add Bai12_Delegate

--- Bai11_ICollection/Program.cs
+++ Bai11_ICollection/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+namespace ICollectionTrongCsharp
+{
+    class Program
+    {
+        public class MyArrayList : ICollection
+        {
+            private object[] lstObj; // mảng giá trị
+            private int count; // số lượng phần tử
+            private const int MAXCOUNT = 100; // số lượng phần tử tối đa
+
+            public MyArrayList()
+            {
+                count = -1;
+                lstObj = new object[MAXCOUNT];
+            }
+
+            public MyArrayList(int count)
+            {
+                this.count = count;
+                lstObj = new object[count];
+            }
+
+            public MyArrayList(Array array)
+            {
+                array.CopyTo(lstObj, 0);
+                count = array.Length;
+            }
+
+            public void CopyTo(Array array, int index)
+            {
+                // thực hiện copy các phần tử trong lstObj từ vị trí index đến cuối sang mảng array.
+                lstObj.CopyTo(array, index);
+            }
+
+            public int Count
+            {
+                get { return count; }
+            }
+
+            public bool IsSynchronized
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public object SyncRoot
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public IEnumerator GetEnumerator()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        static void Main(string[] args) 
+        {
+            
+        }
+    }
+}
---
+++ Bai12_Delagate/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace DelegateTrongCsharp
+{
+    class Program
+    {
+        //Khai bao kieu du lieu myDelagate
+        delegate int myDelegate(string a);
+
+        static void Main(string[] args) 
+        {
+            //Cac bien delagate voi kieu du lieu MyDelagate
+            myDelegate convertToInt = new myDelegate(ConvertStringToInt);
+            myDelegate showString = new myDelegate(ShowString);
+
+            //Multicast 1 delagate
+            myDelegate multicast = convertToInt + showString;
+
+            #region Vidu1
+            string strNumber = "100";
+            int valueConverted = convertToInt(strNumber);
+            Console.WriteLine("Gia tri sau khi chuyen: {0}",valueConverted);
+            Console.WriteLine();
+            #endregion
+
+            #region Multicast delegate
+            Console.WriteLine("Ket qua sau khi chay multicast");
+            multicast(strNumber);
+            Console.WriteLine();
+            #endregion
+
+            #region Call-back Function
+            NhapVaShowTen(showString);
+            #endregion
+
+        }
+        static int ConvertStringToInt(string str)
+        {
+            bool kiemTra;
+            int value = 0;
+            kiemTra = Int32.TryParse(str, out value);
+            if (kiemTra == true)
+            {
+                Console.WriteLine("Da ep kieu du lieu thanh cong");
+            }else
+            {
+                Console.WriteLine("Ep kieu du lieu that bai");
+            }
+            return value;
+        }
+        static int ShowString(string str) 
+        {
+            Console.WriteLine(str);
+            return 0;
+        }
+
+        //Dung delegate cho call-back function
+        static void NhapVaShowTen(myDelegate showTen)
+        {
+            Console.Write("Moi ban nhap ho va ten: ");
+            string ten = Console.ReadLine();
+            showTen(ten);
+        }
+    }
+}
